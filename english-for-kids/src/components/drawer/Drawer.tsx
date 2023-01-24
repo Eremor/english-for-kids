4b1688a012b4bd483431ce37c82de5fdba63fde8
Eremor <artem.s13@gmail.com>
2023-01-24 09:22:53 +0300

    feat: add draw active link

--- english-for-kids/src/components/drawer/Drawer.tsx
+++ english-for-kids/src/components/drawer/Drawer.tsx
@@ -1,12 +1,21 @@
 import React, { FC, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Drawer as CustomDrawer, useMediaQuery } from '@mui/material';
+import { Link, useLocation } from 'react-router-dom';
+import { Drawer as CustomDrawer, useMediaQuery, MenuList, MenuItem, useTheme } from '@mui/material';
 
 import { cards } from '../../data/cards';
 
 const Drawer: FC = () => {
   const matches = useMediaQuery('(min-width: 1440px)');
   const [open, setOpen] = useState(true);
+  const location = useLocation();
+  const theme = useTheme();
+
+  const drawActiveLink = (link: string): string => {
+    const active = theme.palette.secondary.main;
+    const base = theme.palette.primary.main;
+
+    return location.pathname === link ? active : base;
+  };
 
   return (
     <CustomDrawer
@@ -16,14 +25,21 @@ const Drawer: FC = () => {
         sx: { pt: 8, width: matches ? '25%' : '320px', alignItems: 'center' },
       }}
     >
-      <Link to="/" style={{ textDecoration: 'none' }}>
-        main page
-      </Link>
-      {cards.map(({ title, id }) => (
-        <Link key={id} to={`/${title}`} style={{ textDecoration: 'none' }}>
-          {title}
-        </Link>
-      ))}
+      <MenuList sx={{ width: '100%' }}>
+        <MenuItem component={Link} to="/" sx={{ color: `${drawActiveLink('/')}` }}>
+          main page
+        </MenuItem>
+        {cards.map(({ title, id }) => (
+          <MenuItem
+            component={Link}
+            key={id}
+            to={`/${title}`}
+            sx={{ color: `${drawActiveLink(`/${title}`)}` }}
+          >
+            {title}
+          </MenuItem>
+        ))}
+      </MenuList>
     </CustomDrawer>
   );
 };
